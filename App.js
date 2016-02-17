b971663057b5b710186eaf8f639944c1186f073c
emiliano.barbosa <emiliano.barbosa@corp.globo.com>
2016-02-17 11:33:41 -0200

    ADD: using refs to access components

--- App.js
+++ App.js
@@ -1,25 +1,33 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 class App extends React.Component {
 	constructor(){
 		super()
 		this.state = {
-			txt: 'this is a the state txt'
+			red: 0,
+			green: 0,
+			blue: 0
 		}
 		this.update = this.update.bind(this);
 	}
 	update(e){
 		this.setState({
-			txt: e.target.value
+			red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+			green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+			blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
 		})
 	}
 	render(){
 		return (
 			<div>
-				<Widget txt={this.state.txt} update={this.update}/>
-				<Widget txt={this.state.txt} update={this.update}/>
-				<Widget txt={this.state.txt} update={this.update}/>
-				<Widget txt={this.state.txt} update={this.update}/>
-				<Widget txt={this.state.txt} update={this.update}/>
+			{this.state.txt}
+			<hr/>
+				<Slider ref="red" update={this.update} />
+				{this.state.red}
+				<Slider ref="green" update={this.update} />
+				{this.state.green}
+				<Slider ref="blue" update={this.update} />
+				{this.state.blue}
 			</div>
 		);
 	}
@@ -32,6 +40,12 @@ App.propTypes = {
 
 App.defaultProps = {
 	txt: 'this is the default txt'
+}
+
+class Slider extends React.Component {
+	render(){
+		return (<div><label></label><input ref="inp" type="range" min="0" max="255" onChange={this.props.update} /></div>);
+	}
 }
 
 const Widget = (props) => {
