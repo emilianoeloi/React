a8ac95830eeb69eb8afb6cfc2b45fb5d8e013244
emiliano.barbosa <emiliano.barbosa@corp.globo.com>
2016-02-17 17:00:53 -0200

    ADD: higher order components replaces mixins

--- App.js
+++ App.js
@@ -1,59 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-
+let Mixin = InnerComponent => class extends React.Component{
 	constructor(){
 		super();
-		this.state = { increasing: false };
 		this.update = this.update.bind(this);
+		this.state = { val: 0 };
 	}
 
 	update(){
-		console.info('update', this.props.val);
-		ReactDOM.render(
-			<App val={this.props.val+1} />,
-			document.getElementById('app')
-		);
-	}
-
-	componentWillReceiveProps(nextProps){
-		console.info('componentWillReceiveProps', 'nextProps', nextProps);
-		this.setState({increasing: nextProps.val > this.props.val});
-	}
-
-	shouldComponentUpdate(nextProps, nextState){
-		console.info('shouldComponentUpdate','nextProps','nextState',nextProps, nextState);
-		return nextProps.val % 5 == 0;
-	}
-
-	componentDidUpdate(prevProps, prevState){
-		console.info('componentDidUpdate', 'prevProps', 'prevState', prevProps, prevState)
-
+		this.setState({val: this.state.val + 1})
 	}
 
 	componentWillMount(){
 		console.info('componentWillMount');
 	}
 
-	render(){
-		console.info('render');
-		return (
-			<button onClick={this.update}>
-				{this.props.val}
-			</button>
-		)
-	}
-
 	componentDidMount(){
 		console.info('componentDidMount', ReactDOM.findDOMNode(this));
 	}
 
-	componentWillUnmount(){
-		console.info('componentWillUnmount');
+	render(){
+		console.info('render');
+		return (
+			<InnerComponent 
+				update={this.update} 
+				{...this.state}
+				{...this.props}/>
+		)
+	}
+
+	
+}
+
+const Button = (props) => <button onClick={props.update}>{props.txt}-{props.val}</button>
+const Label = (props) => <label onMouseMove={props.update}>{props.txt}-{props.val}</label>
+
+let ButtonMixed = Mixin(Button)
+let LabelMixed = Mixin(Label)
+
+class App extends React.Component {
+
+	render(){
+		console.info('render');
+		return (
+			<div>
+				<ButtonMixed txt="Button" />
+				<LabelMixed txt="Label" />
+			</div>
+		)
 	}
 }
 
-App.defaultProps = {val:0}
+App.defaultProps = {txt:'button'}
 
 export default App
