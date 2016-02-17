8f8a059e6e7f18633141783d0e33114a3f7222ab
emiliano.barbosa <emiliano.barbosa@corp.globo.com>
2016-02-17 18:59:58 -0200

    ADD: dynamically generated components

--- App.js
+++ App.js
@@ -1,78 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
 	constructor(){
 		super();
-		this.state = {
-			red:0
-		}
-		this.update = this.update.bind(this)
+		this.state = {data:[
+			{id:0, name:"Cainã Nuñes"},
+			{id:1, name:"Moises Ribeiro"},
+			{id:2, name:"Alexandre Magno"},
+			{id:3, name:"Cezar Liedke"},
+			{id:4, name:"Guilherme Bruzzi"},
+			{id:5, name:"Emiliano Barbosa"},
+			{id:6, name:"Bayron Quinelato"}
+		]}
 	}
 
-	update(e){
-		this.setState({
-			red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
+	render(){
+
+		let rows = this.state.data.map( person => {
+			return <PersonRow data={person} key={person.id} />
 		})
-	}
 
-	render(){
-		console.info('render');
-		return (
-			<div>
-				<NumInput 
-				ref="red" 
-				min={0}
-				max={255}
-				step={1}
-				val={+this.state.red}
-				label="Red"
-				type="number"
-				update={this.update} />
-				{this.state.red}
-			</div>
-		)
+		return <table>
+		<tbody>{rows}</tbody>
+		</table>
 	}
 }
 
-class NumInput extends React.Component {
-	render(){
-		let label = this.props.label !== '' ?
-			<label>{this.props.label} = {this.props.val}</label> : ''
-		return (
-			<div>
-				<input ref="inp" 
-					type={this.props.type} 
-					min={this.props.min}
-					max={this.props.max}
-					step={this.props.step}
-					defaultValue={this.props.val}
-					ref="inp" 
-					onChange={this.props.update} />
-					{label}
-			</div>	
-		)
-	}
-}
-
-NumInput.propTypes = {
-	min: React.PropTypes.number,
-	max: React.PropTypes.number,
-	step: React.PropTypes.number,
-	val: React.PropTypes.number,
-	label: React.PropTypes.string,
-	update: React.PropTypes.func.isRequired,
-	type: React.PropTypes.oneOf(['number','range'])
-}
-
-NumInput.defaultProps = {
-	min: 0,
-	max: 0,
-	step: 1,
-	val: 0,
-	label: '',
-	type: 'range'
+const PersonRow = (props) => {
+	return <tr>
+		<td>{props.data.id}</td>
+		<td>{props.data.name}</td>
+	</tr>
 }
 
 export default App
