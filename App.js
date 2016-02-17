6be28cff406e3fba4d5f44dfa8ac624f4f9fceca
emiliano.barbosa <emiliano.barbosa@corp.globo.com>
2016-02-17 10:45:30 -0200

    ADD: first component, render method, properties, state basics

--- App.js
+++ App.js
@@ -1,7 +1,38 @@
 import React from 'react';
 class App extends React.Component {
+	constructor(){
+		super()
+		this.state = {
+			txt: 'this is a the state txt',
+			cat: 0
+		}
+	}
+	update(e){
+		this.setState({
+			txt: e.target.value
+		})
+	}
 	render(){
-		return <div>Vai Sabrina!</div>
+		let txt = this.props.txt
+		return (<div>
+			<h1>Vai Sabrina!</h1>
+			<h2>Ai Sávio</h2>
+			<h3>{txt}</h3>
+			<h4>{this.state.txt}</h4>
+			<input type="text" onChange={this.update.bind(this)} />
+			</div>)
 	}
 }
+
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	txt: 'this is the default txt'
+}
+
+// const App = () => <h1>Vai Sávio</h1>
+
 export default App
